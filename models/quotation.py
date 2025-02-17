f8970b23d2c17a33899da6cf2081b7cdddb8fb5a
Pedro <pedrocalezanelyra@gmail.com>
2025-02-17 20:04:59 -0300

    feat: create quotation model

---
+++ models/quotation.py
@@ -0,0 +1,6 @@
+class Quotation:
+    def __init__(self, customer, product, responsable,):
+        self.customer = customer
+        self.product = product
+        self.responsable = responsable
+        self.final_price = None
