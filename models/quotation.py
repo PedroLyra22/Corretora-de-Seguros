4348aea871183d70cee96f8fc7040358ca785465
Pedro <pedrocalezanelyra@gmail.com>
2025-02-18 19:15:42 -0300

    feat: modificação em quotation

--- models/quotation.py
+++ models/quotation.py
@@ -1,6 +1,9 @@
 class Quotation:
-    def __init__(self, customer, product, responsable,):
+    def __init__(self, customer, product, responsable):
         self.customer = customer
         self.product = product
         self.responsable = responsable
-        self.final_price = None
+        self.discount = 0
+
+    def apply_discount(self, value):
+        self.discount = value
