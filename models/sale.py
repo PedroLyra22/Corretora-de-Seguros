d212ef9026f35d7a825542c0e8a1265eb45596ce
Pedro <pedrocalezanelyra@gmail.com>
2025-02-18 19:12:42 -0300

    feat: modificação em sale

--- models/sale.py
+++ models/sale.py
@@ -2,6 +2,9 @@ class Sale:
     def __init__(self, quotation, comission):
         self.quotation = quotation
         self.comission = comission
-        self.discount = None
+        self.final_price = None
+
+    def set_final_price(self):
+        self.final_price = self.quotation.product.price - (self.quotation.product.price * self.quotation.discount)
 
 
